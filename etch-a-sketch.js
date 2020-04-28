62fb76c1934e8f3204edf480c19d68a85d220de6
xaviguasch <xaviguasch@gmail.com>
2020-04-28 21:43:12 +0200

    written a handler for the keys

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -12,18 +12,30 @@ const width = canvas.width
 const height = canvas.height
 
 // create random x and y starting points on the canvas
+let x = Math.floor(Math.random() * width)
+let y = Math.floor(Math.random() * height)
 
 ctx.lineJoin = 'round'
 ctx.lineCap = 'round'
-ctx.lineWidth = 10
+ctx.lineWidth = 30
 
 ctx.beginPath() // start the drawing
-ctx.moveTo(200, 200)
-ctx.lineTo(200, 200)
+ctx.moveTo(x, y)
+ctx.lineTo(x, y)
 ctx.stroke()
 
 // write a draw function
 
 // write a handler for the keys
+function handleKey(e) {
+  if (e.key.includes('Arrow')) {
+    e.preventDefault()
+    console.log(e.key)
+    console.log('handling key')
+  }
+}
 
 // clear/share
+
+// listen for arrow keys
+window.addEventListener('keydown', handleKey)
