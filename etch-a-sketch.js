17f8907975230f7f5f43276f50a7d137eba370ae
xaviguasch <xaviguasch@gmail.com>
2020-04-28 21:16:25 +0200

    set up our canvas for drawing

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -7,6 +7,20 @@ const ctx = canvas.getContext('2d')
 const shakeButton = document.querySelector('.shake ')
 
 // Setup our canvas for drawing
+// make a variable called height and width from the same properties on our canbas.
+const width = canvas.width
+const height = canvas.height
+
+// create random x and y starting points on the canvas
+
+ctx.lineJoin = 'round'
+ctx.lineCap = 'round'
+ctx.lineWidth = 10
+
+ctx.beginPath() // start the drawing
+ctx.moveTo(200, 200)
+ctx.lineTo(200, 200)
+ctx.stroke()
 
 // write a draw function
 
