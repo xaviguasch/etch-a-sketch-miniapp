517ed3b56bec3233567b896ae64c47d4bb9a3141
xaviguasch <xaviguasch@gmail.com>
2020-04-28 20:53:16 +0200

    selected the elements on the page

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,5 +1,11 @@
 // Select the elements on the page - canvas, shake button
 
+const canvas = document.querySelector('#etch-a-sketch')
+
+const ctx = canvas.getContext('2d')
+
+const shakeButton = document.querySelector('.shake ')
+
 // Setup our canvas for drawing
 
 // write a draw function
