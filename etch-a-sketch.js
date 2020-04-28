74f7943e9a2011b935456acc579e79b33066358b
xaviguasch <xaviguasch@gmail.com>
2020-04-28 22:05:51 +0200

    implemented drawing function

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -6,6 +6,8 @@ const ctx = canvas.getContext('2d')
 
 const shakeButton = document.querySelector('.shake ')
 
+const MOVE_AMOUNT = 10
+
 // Setup our canvas for drawing
 // make a variable called height and width from the same properties on our canbas.
 const width = canvas.width
@@ -17,7 +19,7 @@ let y = Math.floor(Math.random() * height)
 
 ctx.lineJoin = 'round'
 ctx.lineCap = 'round'
-ctx.lineWidth = 30
+ctx.lineWidth = MOVE_AMOUNT
 
 ctx.beginPath() // start the drawing
 ctx.moveTo(x, y)
@@ -25,13 +27,40 @@ ctx.lineTo(x, y)
 ctx.stroke()
 
 // write a draw function
+function draw({ key }) {
+  // start the path
+  ctx.beginPath()
+  ctx.moveTo(x, y)
+
+  // move our x and y values depending on what the user did
+
+  switch (key) {
+    case 'ArrowUp':
+      y = y - MOVE_AMOUNT
+      break
+    case 'ArrowDown':
+      y = y + MOVE_AMOUNT
+      break
+    case 'ArrowRight':
+      x = x + MOVE_AMOUNT
+      break
+    case 'ArrowLeft':
+      x = x - MOVE_AMOUNT
+      break
+
+    default:
+      break
+  }
+
+  ctx.lineTo(x, y)
+  ctx.stroke()
+}
 
 // write a handler for the keys
 function handleKey(e) {
   if (e.key.includes('Arrow')) {
     e.preventDefault()
-    console.log(e.key)
-    console.log('handling key')
+    draw({ key: e.key })
   }
 }
 
